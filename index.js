26cd7e1eabed3bfd4934633b178819eaaf2fd4e4
Elvis Onobhayedo <elvisonob@yahoo.com>
2022-02-22 21:29:49 +0000

    review guide

--- index.js
+++ index.js
@@ -1,86 +1,86 @@
-const formEl = document.querySelector('#select-state-form')
-const inputEl = document.querySelector('#select-state')
-const ulEl = document.querySelector('#breweries-list')
-const filterEl = document.querySelector('#filter-by-type')
-const stateSearch = document.querySelector('#select-state')
+const validTypes = [
+    'micro',
+    'brewpub',
+    'regional'
+
+]
+
 
 const state = {
     breweries: [],
-    //type: []
+    filterType: '',
+    filterState: ''
+}
 
+function setBreweries(breweries) {
+    state.breweries = breweries.filter(function(brewery) {
+        return validTypes.includes(brewery.brewery_type)
+
+    })
 }
 
 
-let url = 'https://api.openbrewerydb.org/breweries?by_state='
+console.log('hello')
 
+const inputEl = document.querySelector('#select-state')
+const formEl = document.querySelector('#select-state-form')
+const filterEl = document.querySelector('#filter-by-type')
+
+filterEl.addEventListener('change', function() {
+    state.filterType = filterEl.value
+    fetchBreweries()
+})
 
 formEl.addEventListener('submit', function(e) {
+    state.filterState = inputEl.value
     e.preventDefault()
-    const searchValue = inputEl.value
-        //console.log('searchValue', searchValue)
-    fetch(url + searchValue)
+    fetchBreweries()
+
+
+})
+
+
+function fetchBreweries() {
+    if (state.filterState === '') {
+        return
+    }
+    let url = 'https://api.openbrewerydb.org/breweries?by_state=' + state.filterState
+    if (state.filterType !== '') {
+        url += '&by_type=' + state.filterType
+    }
+    fetch(url)
         .then(function(response) {
-            //console.log('in the response', response)
             return response.json()
         })
         .then(function(breweries) {
             state.breweries = breweries
-            console.log('breweries', state.breweries)
             render()
-
-
         })
-})
-
+}
 
 function render() {
-    clear()
-    renderBreweries()
-    changeFilter()
-}
-
-function clear() {
-    inputEl.innerHTML = ''
-    inputEl.value = ''
-}
-
-function renderBreweries() {
+    const breweriesListEl = document.querySelector('#breweries-list')
+    breweriesListEl.innerHTML = ''
     for (const type of state.breweries) {
-        const li = document.createElement('li')
-        li.innerHTML = `
-    <h2>${type.name}</h2>
-    <div class="type">${type.brewery_type}</div>
-    <section class="address">
-      <h3>Address:</h3>
-      <p>${type.street}</p>
-      <p><strong>${type.city},${type.postal_code}</strong></p>
-    </section>
-    <section class="phone">
-      <h3>Phone:</h3>
-      <p>${type.phone}</p>
-    </section>
-    <section class="link">
-      <a href=${type.website_url} target="_blank">Visit Website</a>
-    </section>`
-        ulEl.append(li)
-    }
-}
+        console.log('type', type)
+        const breweryEl = document.createElement('li')
+        breweryEl.innerHTML = `
+        <h2>${type.name}</h2>
+        <div class="type">${type.brewery_type}</div>
+        <section class="address">
+          <h3>Address:</h3>
+          <p>${type.street}</p>
+          <p><strong>${type.city}, ${type.postal_code}</strong></p>
+        </section>
+        <section class="phone">
+          <h3>Phone:</h3>
+          <p>${type.phone}</p>
+        </section>
+        <section class="link">
+          <a href="${type.website_url}" target="_blank">Visit Website</a>
+        </section>`
 
-function changeFilter() {
-    const url2 = 'https://api.openbrewerydb.org/breweries?by_type='
-    filterEl.addEventListener('change', function(e) {
-        e.preventDefault()
-        const filterType = filterEl.value
-        console.log('checkingfilter', filterType)
-        fetch(`https://api.openbrewerydb.org/breweries?by_state=${stateSearch.value}&by_type=${filterType}`)
-            .then(function(response) {
-                return response.json()
-            })
-            .then(function(type) {
-                state.breweries = type
-                console.log('filter', type)
-                render()
-            })
-    })
+        breweriesListEl.append(breweryEl)
+    }
+
 }
-changeFilter()
